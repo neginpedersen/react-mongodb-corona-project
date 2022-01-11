0caaf7fdd2cbee436030eeb1663de125a94b18f6
Anders <neginpedersen@gmail.com>
2022-01-11 14:49:08 +0100

    cleaning the code

--- backend/routes/story.route.js
+++ backend/routes/story.route.js
@@ -43,34 +43,8 @@ router.route('/edit-story/:id').get((req, res) => {
 
 // Update Story
 router.route('/update-story/:id').put((req, res, next) => {
-  function cleanStringify(object) {
-    if (object && typeof object === 'object') {
-        object = copyWithoutCircularReferences([object], object);
-    }
-    return JSON.stringify(object);
+  
 
-    function copyWithoutCircularReferences(references, object) {
-        var cleanObject = {};
-        Object.keys(object).forEach(function(key) {
-            var value = object[key];
-            if (value && typeof value === 'object') {
-                if (references.indexOf(value) < 0) {
-                    references.push(value);
-                    cleanObject[key] = copyWithoutCircularReferences(references, value);
-                    references.pop();
-                } else {
-                    cleanObject[key] = '###_Circular_###';
-                }
-            } else if (typeof value !== 'function') {
-                cleanObject[key] = value;
-            }
-        });
-        return cleanObject;
-    }
-}
-
-  console.log('id'+cleanStringify(req));
-  // storySchema.findByIdAndUpdate(req.params.id, {name:req.params.name,story:req.params.story
 storySchema.findByIdAndUpdate(req.params.id, {name:req.body.name,story:req.body.story,country:req.body.story
 
 }, function (err, response) {
--- src/components/story-list-editable.component.js
+++ src/components/story-list-editable.component.js
@@ -50,7 +50,6 @@ CancelEdit()
 {
   this.setState({isEditing:false});
   this.setState({newData:{name:'',country:'',story:'',id:''}});
-//  console.log(this.state.newData);
 }
 
 SetnewData(id,field,data){
@@ -59,7 +58,6 @@ SetnewData(id,field,data){
   this.setState({ newData: {...this.state.newData, [field]:data ,'id':id}});
 }
 
- // console.log(this.state.newData);
 }
 
 SaveStory(story_id){
@@ -69,8 +67,6 @@ const putdata={
     country: this.state.newData.country,
     story: this.state.newData.story
   }
- console.log('from7'+JSON.stringify(this.state.newData));
-
   axios.put('http://localhost:4000/storys/update-story/' + story_id,putdata,{
     headers: {
       "x-access-token": "token-value",
@@ -78,10 +74,8 @@ const putdata={
   })
       .then((res) => {
        console.log(res);
-        //this.setState(this.state.storys);
         console.log('story successfully updated!');
       }).catch((error) => {
-        //  console.log(error)
       })
 
 }
@@ -159,253 +153,4 @@ render() {
 
   );
 }
-
-
-
- 
-
-
-
 }
-/*
-
-class EditableRow extends Component {
- 
-
-  render() {
-    return (
-      <tr>
-      <td>
-        <input type='text' name={this.props.cellData.type} id={this.props.cellData.id} value={this.props.cellData.value} onChange={this.props.onStoryTableUpdate}/>
-      </td>
-      <td>
-        <input type='text' name={this.props.cellData.type} id={this.props.cellData.id} value={this.props.cellData.value} onChange={this.props.onStoryTableUpdate}/>
-      </td>
-      <td>
-        <input type='text' name={this.props.cellData.type} id={this.props.cellData.id} value={this.props.cellData.value} onChange={this.props.onStoryTableUpdate}/>
-      </td>
-      </tr>
-    );
-
-  }
-  
-
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-    this.state.filterText = "";
-    this.state.storys = [];
-  }
-  componentDidMount() {
-    axios.get('http://localhost:4000/storys/',{ crossDomain: true })
-      .then(res => {  
-        this.setState({
-          storys: res.data
-        });
-        })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
-
-  handleUserInput(filterText) {
-    this.setState({filterText: filterText});
-  };
-
-  handleRowSave(story) {
-    
-    let editedIndex = this.state.storys.indexOf( story._id);
-   // console.log(editedIndex);
-    axios.put('http://localhost:4000/storys/update-story/' + story._id, story)
-    .then((res) => {
-      console.log('story successfully updated')
-    }).catch((error) => {
-     // console.log(error)
-    })
-    
-  };
-
-  handleRowDel(story) {
-    var index = this.state.storys.indexOf(story);
-    axios.delete('http://localhost:4000/storys/delete-story/' + story._id)
-        .then((res) => {
-          //  console.log('story successfully deleted!')
-        }).catch((error) => {
-          //  console.log(error)
-        })
-    this.state.storys.splice(index, 1);
-    this.setState(this.state.storys);
-  };
-
-  handleAddEvent(evt) {
-    var id = (+ new Date() + Math.floor(Math.random() * 999999)).toString(36);
-    var story = {
-      id: id,
-      name: "",
-      country: "",
-      story: ""
-      }
-    this.state.storys.push(story);
-    this.setState(this.state.storys);
-
-  }
-
-  handleStoryTable(evt) {
-    var item = {
-      _id: evt.target.id,
-      country: evt.target.country,
-      story: evt.target.story
-    };
-console.log(evt);
-
-    let editedIndex = this.state.storys.indexOf( item._id);
-    console.log(editedIndex);
-    axios.put('http://localhost:4000/storys/update-story/' + item._id, item.story)
-    .then((res) => {
-      console.log(res.data);
-      var storys = this.state.storys.slice();
-  var newstorys = storys.map(function(story) {
-
-    for (var key in story) {
-      if (key === item.name && story.id === item._id) {
-        story[key] = item.value;
-
-      }
-    }
-    return story;
-  });
-    this.setState({storys:newstorys});
-      console.log('story successfully updated')
-    }).catch((error) => {
-      console.log(error)
-    })
-
-  
-
-  //  console.log(this.state.storys);
-  };
-  render() {
-
-    return (
-      <div>
-        <SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput.bind(this)}/>
-        <StoryTable onStoryTableUpdate={this.handleStoryTable.bind(this)} onRowAdd={this.handleAddEvent.bind(this)} onRowDel={this.handleRowDel.bind(this)} onRowSave={this.handleRowSave.bind(this)} storys={this.state.storys} filterText={this.state.filterText}/>
-      </div>
-    );
-
-  }
-
-}
-class SearchBar extends Component {
-  handleChange() {
-    this.props.onUserInput(this.refs.filterTextInput.value);
-  }
-  render() {
-    return (
-      <div>
-
-        <input type="text" placeholder="Search..." value={this.props.filterText} ref="filterTextInput" onChange={this.handleChange.bind(this)}/>
-
-      </div>
-
-    );
-  }
-
-}
-
-class StoryTable extends Component {
-
-  render() {
-    var onStoryTableUpdate = this.props.onStoryTableUpdate;
-    var rowDel = this.props.onRowDel;
-    var rowSave = this.props.onRowSave;
-    var filterText = this.props.filterText;
-    var story = this.props.storys.map(function(story) {
-      if (story.name.indexOf(filterText) === -1) {
-        return;
-      }
-      return (<StoryRow onStoryTableUpdate={onStoryTableUpdate} story={story} onDelEvent={rowDel.bind(this)} onSaveEvent={rowSave.bind(this)} key={story.id}/>)
-    });
-    return (
-      <div>
-
-
-      <button type="button" onClick={this.props.onRowAdd} className="btn btn-success pull-right">Add</button>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>country</th>
-              <th>story</th>
-              
-            </tr>
-          </thead>
-
-          <tbody>
-            {story}
-
-          </tbody>
-
-        </table>
-      </div>
-    );
-
-  }
-
-}
-
-class StoryRow extends Component {
-  onDelEvent() {
-    this.props.onDelEvent(this.props.story);
-
-  }
-  onSaveEvent(){
-    this.props.onSaveEvent(this.props.story);
-  }
-  render() {
-
-    return (
-      <tr className="eachRow">
-        <EditableCell onStoryTableUpdate={this.props.onStoryTableUpdate} cellData={{
-          "type": "name",
-          value: this.props.story.name,
-          id: this.props.story.id
-        }}/>
-        <EditableCell onStoryTableUpdate={this.props.onStoryTableUpdate} cellData={{
-          type: "country",
-          value: this.props.story.country,
-          id: this.props.story.id
-        }}/>
-        <EditableCell onStoryTableUpdate={this.props.onStoryTableUpdate} cellData={{
-          type: "story",
-          value: this.props.story.story,
-          id: this.props.story.id
-        }}/>
-        
-        <td className="del-cell">
-          <input type="button" onClick={this.onDelEvent.bind(this)} value="X" className="del-btn"/>
-        </td>
-        <td className="edit-cell">
-          <input type="button" onClick={this.onSaveEvent.bind(this)} value="Save" className="save-btn"/>
-        </td>
-      </tr>
-    );
-
-  }
-
-}
-class EditableCell extends Component {
- 
-
-  render() {
-    return (
-      <td>
-        <input type='text' name={this.props.cellData.type} id={this.props.cellData.id} value={this.props.cellData.value} onChange={this.props.onStoryTableUpdate}/>
-      </td>
-    );
-
-  } 
-
-}*/
-
