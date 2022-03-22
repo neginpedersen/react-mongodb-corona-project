54da230f08b69fcb2d0615ad1f44f00e616b8db0
neginpedersen <neginpedersen@gmail.com>
2022-03-22 11:35:56 +0100

    code optimzation create story and create story one time

--- src/components/board-admin.component.js
+++ src/components/board-admin.component.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import UserService from "../services/user.service";
 import EventBus from "../common/EventBus";
 
--- src/components/modal.component.js
+++ src/components/modal.component.js
@@ -1,25 +1,44 @@
 import React, { Component } from "react";
 import CreateStory from './create-story.component';
 import CreateStoryCaptcha from './create-story-captcha.component';
-
 import '../sass/layout/layout.scss';
+import { connect } from "react-redux";
+import Login from "./login.component";
 
-export default class Modal extends Component {
 
-  
-   
-    render() {
-        return (
-            <div className={this.props.show ? "mymodal display-block" : "mymodal display-none"}>
-              <section className="mymodal-main">
-              <button className='close-modal' onClick={this.props.handleclose} >
-                  x
-                </button>
-                  {this.props.modaltype==1? <CreateStory></CreateStory> :<CreateStoryCaptcha></CreateStoryCaptcha> }
-                
-                
-              </section>
-            </div>
-          );
+class Modal extends Component {
+
+  render() {
+    const { isLoggedIn, message } = this.props;
+    let modal1;
+    if (this.props.modaltype == 1) {
+      isLoggedIn==true ? modal1= <CreateStory></CreateStory>:modal1=<Login></Login>;
     }
+    else if (this.props.modaltype==2) {
+        modal1=  <CreateStoryCaptcha></CreateStoryCaptcha>;
+    }
+
+    return (
+      <div className={this.props.show ? "mymodal display-block" : "mymodal display-none"}>
+        <section className="mymodal-main">
+          <button className='close-modal' onClick={this.props.handleclose} >
+               x
+          </button>
+            {modal1}
+        </section>
+    </div>
+
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  const { isLoggedIn } = state.auth;
+  const { message } = state.message;
+  return {
+    isLoggedIn,
+    message
+  };
+}
+
+export default connect(mapStateToProps)(Modal);
