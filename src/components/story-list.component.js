ab2a397b60a4466e1960aa407b8b39296b29fec2
neginpedersen <neginpedersen@gmail.com>
2022-03-22 15:58:23 +0100

    simple pagination without css is added to the storylist

--- src/components/story-list.component.js
+++ src/components/story-list.component.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import StoryTableRow from './StoryTableRow';
-
+import Pagination from './pagination.component';
 
 export default class StoryList extends Component {
 
   constructor(props) {
     super(props)
+    var exampleItems = [...Array(150).keys()].map(i => ({ id: (i+1), name: 'Item ' + (i+1) }));
     this.state = {
-      storys: []
+      storys: [],
+      exampleItems: exampleItems,
+      pageOfItems: []
     };
+    this.onChangePage = this.onChangePage.bind(this);
+
   }
+
+  onChangePage(pageOfItems) {
+    // update state with new page of items
+    this.setState({ pageOfItems: pageOfItems });
+}
 
   componentDidMount() {
     axios.get('http://localhost:4000/storys/',{ crossDomain: true })
-      .then(res => {  
+      .then(res => {
         this.setState({
           storys: res.data
         });
@@ -34,14 +44,17 @@ export default class StoryList extends Component {
   render() {
     return (
       <div>
-   
-    {this.state.storys.map((person,i) => (
-       <div className="history-contianer">
-         <h5>{person.name} from {person.country} </h5> 
-    <p key={i}>{person.story}</p>
-    </div>
-  ))}
 
+   <div className="container">
+                    <div className="text-center">
+
+                        {this.state.pageOfItems.map(item =>
+                            <div key={item.id}> name: {item.name} from:  {item.country} wrote: {item.story}</div>
+
+                        )}
+                        <Pagination items={this.state.storys} onChangePage={this.onChangePage} />
+                    </div>
+                </div>
       </div>);
   }
 }
